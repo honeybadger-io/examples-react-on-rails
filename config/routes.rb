69ccc6bad3af980f4894937f996fb556c2d596a2
Joshua Wood <josh@joshuawood.net>
2020-05-15 09:06:52 -0700

    delete more boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: "router#index"
-  get "/*all" => "router#index", as: :router
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/*all" => "router#index", :as => :router
 end
